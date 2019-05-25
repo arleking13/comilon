a30e603f3ec2d78a7b5da0a34c4d4b35fdd66889
Felipe Ortiz <felipe.ortiz.oyarzun@gmail.com>
2019-05-25 00:13:56 -0400

    algo

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include_once 'view/layouts/header.php'; ?>
+
+  <section class="page-section clearfix">
+    <div class="container">
+      <div class="intro">
+        <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/intro.jpg" alt="">
+        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Pedido a domicilio?</span>
+            <span class="section-heading-lower">Lalamove delivery</span>
+          </h2>
+          <p class="mb-3">
+            Vive la experiencia de recibir en tus manos, tu plato o comida favorita con una atencion inmediata y sin moverte de tu hogar! Contamos con un equipo de persona preparadas y capacitadas para cumplir con los tiempos acoordados!
+          </p>
+          <div class="intro-button mx-auto">
+            <a class="btn btn-primary btn-xl" href="#">Buscanos!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="page-section cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <div class="cta-inner text-center rounded">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-upper">Productos</span>
+              <span class="section-heading-lower">Prefiere nuestras marcas</span>
+            </h2>
+            <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php include_once 'view/layouts/footer.php'; ?>
